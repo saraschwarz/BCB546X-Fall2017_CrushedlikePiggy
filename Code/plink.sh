89f7c6858dc1c254c8de5d5b0c7179534b9135f4
Piatscheck <finnp@iastate.edu>
2017-12-08 17:50:09 -0600

    done

--- Code/plink.sh
+++ Code/plink.sh
@@ -9,10 +9,4 @@ plink --ped DG_Pig.ped --map DG_Pig.map --recode --maf 0.05 --out Pig.filtered.0
 plink --ped DG_Boar.ped --map DG_Boar.map --maf 0.005 --recode --out Boar.0.05
 plink --ped DG_Pig.ped --map DG_Pig.map --maf 0.005 --recode --out Pig.0.05
 
-
-New_Boar <- read.pedfile("Boar.0.05.ped", snps = "Boar.0.05.map")
-New_Pig <- read.pedfile("Pig.0.05.ped", snps = "Pig.0.05.map")
-
-###
-
-### To copy:  scp finnp@hpc-class.its.iastate.edu:~/in_class/final_project/Pig.f* ~/Desktop/Final\ project/
+### To copy:  scp UserID@hpc-class.its.iastate.edu:~/PathToTheData/Pig.f* ~/PathToTheWorkingDirectoryOnLocalMachine
