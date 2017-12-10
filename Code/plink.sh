ab59e03cc2f2c76b13d1b031f56fd94dabd2dbc6
Piatscheck <finnp@iastate.edu>
2017-12-09 23:49:02 -0600

    plink fixed

--- Code/plink.sh
+++ Code/plink.sh
@@ -3,9 +3,7 @@ clear
 module load plink
 plink --ped DG_Boar.ped --map DG_Boar.map --recode --maf 0.005 --max-maf 0.03 --out Board.filtered.0.005
 plink --ped DG_Boar.ped --map DG_Boar.map --recode --maf 0.05 --out Board.filtered.0.05
-
 plink --ped DG_Pig.ped --map DG_Pig.map --recode --maf 0.05 --out Pig.filtered.0.05
-
 plink --ped DG_Boar.ped --map DG_Boar.map --maf 0.005 --recode --out Boar.0.05
 plink --ped DG_Pig.ped --map DG_Pig.map --maf 0.005 --recode --out Pig.0.05
 
